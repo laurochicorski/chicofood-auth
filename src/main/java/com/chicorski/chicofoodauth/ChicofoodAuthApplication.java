bab63609b20d0e1e4717b118ebc7f05abe32f156
Lauro <laurocoelhoc@gmail.com>
2021-09-07 12:26:33 -0300

    Commit initial

---
+++ src/main/java/com/chicorski/chicofoodauth/ChicofoodAuthApplication.java
@@ -0,0 +1,13 @@
+package com.chicorski.chicofoodauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChicofoodAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChicofoodAuthApplication.class, args);
+    }
+
+}
