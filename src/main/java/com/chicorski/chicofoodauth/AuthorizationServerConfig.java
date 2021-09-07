d340c059f5c45882ec60c19eef4d9533b605b039
Lauro <laurocoelhoc@gmail.com>
2021-09-07 17:30:31 -0300

    Configurando a validade e não reutilização de Refresh Tokens

--- src/main/java/com/chicorski/chicofoodauth/AuthorizationServerConfig.java
+++ src/main/java/com/chicorski/chicofoodauth/AuthorizationServerConfig.java
@@ -31,12 +31,15 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                         .secret(passwordEncoder.encode("web123"))
                         .authorizedGrantTypes("password", "refresh_token")
                         .scopes("write", "read")
-                        .accessTokenValiditySeconds(60 * 60 * 6)
+                        .accessTokenValiditySeconds(60 * 60 * 6) // 6 horas
+                        .refreshTokenValiditySeconds(60 * 60 * 24 * 7) // 7 Dias
                 .and()
                     .withClient("chicofood-api")
                         .secret(passwordEncoder.encode("api123"))
                         .authorizedGrantTypes("password")
-                        .scopes("write", "read");
+                        .scopes("write", "read")
+                        .accessTokenValiditySeconds(60 * 60 * 6) // 6 horas
+                        .refreshTokenValiditySeconds(60 * 60 * 24 * 7); // 7 Dias
     }
 
     @Override
@@ -48,6 +51,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints
                 .authenticationManager(authenticationManager)
-                .userDetailsService(userDetailsService);
+                .userDetailsService(userDetailsService)
+                .reuseRefreshTokens(false);
     }
 }
